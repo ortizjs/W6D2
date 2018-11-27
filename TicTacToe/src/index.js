9d7bdb89b5c5f54f90e618f8e25ad85cfed90d49
ortizjs <jonnatano94@gmail.com>
2018-11-27 15:23:21 -0800

    Add in bind events makes move

--- TicTacToe/src/index.js
+++ TicTacToe/src/index.js
@@ -6,6 +6,7 @@ const Game = require('../solution/game.js');// require appropriate file
     $el = $('.ttt');
     let game = new Game();
     let view = new View(game, $el);
+    view.bindEvents()
     // $el.append(view)
   });
   
--- TicTacToe/src/ttt-view.js
+++ TicTacToe/src/ttt-view.js
@@ -3,21 +3,41 @@ class View {
     this.game = game;
     this.el = $el;
     $el.append(this.setupBoard());
-    console.log(this)
+    console.log(this);
   }
 
-  bindEvents() {}
+  bindEvents() {
+    let $board = $('ul.grid');
+    console.log('hello');
+    $board.on("click", 'li', (e) => {
+      let $square = $(e.target);
+      if ($square.data('click')) {
+        alert('Invalid move! Foo');
+      } else {
+        this.makeMove($square);
+        $square.text(this.game.currentPlayer);
+        this.game.playMove($square.data('pos'));
+        $square.data('click', true)
+      }
+    });
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    $square.removeClass();
+    $square.addClass(this.game.currentPlayer);
+  }
 
   setupBoard() {
     let $board = $('<ul>');
     $board.addClass('grid');
-    for (let i = 0; i < 9; i++) {
-      let $square = $('<li>');
-      $square.data('pos', i);
-      $square.addClass('square');
-      $board.append($square);
+    for (let x = 0; x < 3; x++) {
+      for (var y = 0; y < 3; y++) {
+        let $square = $('<li>');
+        $square.data('pos', [x,y]);
+        $square.data('click', false);
+        $square.addClass('square');
+        $board.append($square);
+      }
     }
     return $board;
   }
