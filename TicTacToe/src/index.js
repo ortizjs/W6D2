8786363810b1ac55e47cc8ee165def042e26356d
ortizjs <jonnatano94@gmail.com>
2018-11-27 12:23:27 -0800

    Add setup board

--- TicTacToe/src/index.js
+++ TicTacToe/src/index.js
@@ -1,6 +1,9 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js');// require appropriate file
+const Game = require('../solution/game.js');// require appropriate file
 
   $(() => {
     // Your code here
+    $el = $('.ttt');
+    let game = new Game();
+    let view = new View(game, $el);
   });
--- TicTacToe/src/ttt-view.js
+++ TicTacToe/src/ttt-view.js
@@ -5,7 +5,15 @@ class View {
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    let $board = $('ul');
+    $board.addClass('grid');
+    for (var i = 0; i < 9; i++) {
+      var $square = $('li');
+      $square.addClass('square');
+      $board.append($square);
+    }
+  }
 }
 
 module.exports = View;
